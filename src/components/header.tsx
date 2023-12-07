a57a53860be6347a5edb6f34a2091b5a9229007b
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-12-06 16:46:30 -0800

    minor fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { RiMenu4Line, RiCloseCircleLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom';
 const Header = () => {
     // Manage header bg change on scroll
     const [scrollY, setScrollY] = useState(0);
@@ -38,6 +38,9 @@ const Header = () => {
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
       };
+
+      const location = useLocation();
+      const isHome = location.pathname === '/';
     return ( 
         <header className={`fixed w-full py-3 px-[5%] flex items-center justify-between z-1000 shadow-lg z-50 border-b border-1 border-gray-600 ${headerBackground}`}>
             <Link to="/"  >
@@ -45,12 +48,21 @@ const Header = () => {
             </Link>
 
             <div className={`${isMenuOpen ? 'flex' : 'hidden'} absolute top-[10px] border-purple lg:border-none border right-4 lg:right-0 lg:top-0 bg-darkpurple lg:bg-transparent rounded-xl lg:rounded-none shadow-sm shadow-gray-600 lg:shadow-none lg:p-0 p-10 lg:relative lg:flex flex-col lg:flex-row items-start lg:items-center text-white gap-10 transition-transform transform`}>
+              {isHome ?
                 <nav className="text-[16px] font-medium flex flex-col items-start lg:items-center lg:flex-row gap-2  z-1000">
                     <a href="#timeline" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">Timeline</a>
                     <a href="#overview" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">Overview</a>
                     <a href="#faqs" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">FAQs</a>
                     <Link to="/contact" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">Contact</Link>
                 </nav>
+                :
+                <nav className="text-[16px] font-medium flex flex-col items-start lg:items-center lg:flex-row gap-2  z-1000">
+                    <a href="/#timeline" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">Timeline</a>
+                    <a href="/#overview" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">Overview</a>
+                    <a href="/#faqs" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">FAQs</a>
+                    <Link to="/contact" className="p-3 rounded-sm hover:text-pink hover:bg-purpleblur slow-transition">Contact</Link>
+                </nav>
+              }
 
                 <Link to="/register" className="btn py-3 px-10 text-[16px] font-medium cursor-pointer rounded-sm">Register</Link>
 
